da8f4ff4efbda86d811d55c148572ef3a0a239ef
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-04-11 19:57:08 -0400

    Contact page func, sticky foote, other styles

---
+++ src/contact.js
@@ -0,0 +1,13 @@
+"use strict"
+
+export default function generateContact() {
+    const main = document.getElementById('main');
+    main.replaceChildren();
+    const location = document.createElement('p');
+    const phoneNumber = document.createElement('p');
+    const hours = document.createElement('p');
+    location.innerText = `800 Main St, New York, New York`;
+    phoneNumber.innerText = `555-555-0012`;
+    hours.innerText = `Monday - Friday: 10:00am - 11:00pm\nSunday-Saturday: 10:00am - 3:00am`;
+    main.append(location, phoneNumber, hours);
+}
